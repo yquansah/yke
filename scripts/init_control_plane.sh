5a1331c464512383871ee8e8f72ae0f40286570d
Yoofi Quansah <ybquansah@gmail.com>
2024-12-22 21:45:07 -0800

    feat: more modifications to scripts

--- scripts/init_control_plane.sh
+++ scripts/init_control_plane.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+HOSTNAME=$(aws ec2 describe-instances --filters "Name=tag:Component,Values=control-plane-node" --output text --query "Reservations[].Instances[].PublicIpAddress")
 LOAD_BALANCER_HOST=$(aws elbv2 describe-load-balancers --output text --query "LoadBalancers[0].DNSName")
 LOAD_BALANCER_PORT=6443
 
 if [ -n "$POD_CIDR" ]; then
-  for hostname in $(aws ec2 describe-instances --filters "Name=tag:Component,Values=control-plane-node" --output text --query "Reservations[0].Instances[0].PublicIpAddress" | xargs); do
-    ssh ubuntu@"$hostname" \
-      "LOAD_BALANCER_HOST=$LOAD_BALANCER_HOST LOAD_BALANCER_PORT=$LOAD_BALANCER_PORT POD_CIDR=$POD_CIDR bash -s" <<"EOF"
+  ssh ubuntu@"$HOSTNAME" \
+    "LOAD_BALANCER_HOST=$LOAD_BALANCER_HOST LOAD_BALANCER_PORT=$LOAD_BALANCER_PORT POD_CIDR=$POD_CIDR bash -s" <<"EOF" >init_output.txt
+#!/bin/bash
 NODENAME=$(hostname -s)
 sudo kubeadm init --control-plane-endpoint="$LOAD_BALANCER_HOST:$LOAD_BALANCER_PORT" --apiserver-cert-extra-sans="$LOAD_BALANCER_HOST" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://docs.projectcalico.org/manifests/calico.yaml
 EOF
-  done
 else
   echo "Please provide POD_CIDR environment variable."
   exit 1
--- scripts/join_worker_node.sh
+++ scripts/join_worker_node.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 LOAD_BALANCER_HOST=$(aws elbv2 describe-load-balancers --output text --query "LoadBalancers[0].DNSName")
+LOAD_BALANCER_PORT=6443
 
-if [ -n "$CONTROL_PLANE_HOST" ] && [ -n "$CONTROL_PLANE_PORT" ] && [ -n "$TOKEN" ] && [ -n "$CA_CERT_HASH" ]; then
-  sudo kubeadm join "$CONTROL_PLANE_HOST":"$CONTROL_PLANE_PORT" --token "$TOKEN" --discovery-token-ca-cert-hash "$CA_CERT_HASH"
+if [ -n "$TOKEN" ] && [ -n "$CA_CERT_HASH" ]; then
+  for hostname in $(aws ec2 describe-instances --filters "Name=tag:Component,Values=worker-node" --output text --query "Reservations[].Instances[].PublicIpAddress"); do
+    ssh ubuntu@"$hostname" \
+      "LOAD_BALANCER_HOST=$LOAD_BALANCER_HOST LOAD_BALANCER_PORT=$LOAD_BALANCER_PORT TOKEN=$TOKEN CA_CERT_HASH=$CA_CERT_HASH bash -s" <<"EOF"
+#!/bin/bash
+sudo kubeadm join "$LOAD_BALANCER_HOST:$LOAD_BALANCER_PORT" --token "$TOKEN" --discovery-token-ca-cert-hash "$CA_CERT_HASH"
+EOF
+  done
+else
+  echo "Either TOKEN or CA_CERT_HASH is not defined."
 fi
